07b9ce049ee27dbc5c2e8e63c1ea2ebf56e57f19
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-26 14:47:18 +0100

    unpdated link.rb

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,8 +10,7 @@ class Link
 
 end
 
-DataMapper.setup(:default,
-                  "postgres://localhost/bookmarker_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmarker_manager_#{ENV['RACK_ENV']}")
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
