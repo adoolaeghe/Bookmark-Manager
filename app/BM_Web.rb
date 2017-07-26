75bbdd6ff9f955a94a04d1e948435c3fa1cb1a9d
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-26 14:55:21 +0100

    add homepage

--- app/BM_Web.rb
+++ app/BM_Web.rb
@@ -4,7 +4,10 @@ require 'sinatra/base'
 require_relative 'models/link'
 
 class BMWeb < Sinatra::Base
-
+  get '/' do
+    redirect '/links'
+  end
+  
   get '/links' do
     @links = Link.all
     erb :'links/index'
